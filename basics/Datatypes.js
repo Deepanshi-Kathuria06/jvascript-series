b3ab31881309441b9e90f46185b62879bf5e946e
Deepanshi Kathuria <145685048+Deepanshi-Kathuria06@users.noreply.github.com>
2025-06-12 19:53:24 +0000

    Javascript Datatypes

---
+++ basics/Datatypes.js
@@ -0,0 +1,25 @@
+// Datatype in javascript
+
+"use strict";  // treat all the js  code as per newer version
+
+// alert("hello world"); // we are using node js, not browser
+
+// make the code readable dont messup with the code 
+
+// ecmaScript 6 (ES6) is the latest version of JavaScript uaiing the standards for the js not the js 
+
+//  documentations MDN, TC39s
+
+// Dtatatypes :-  types of data that can be stored in a variable
+
+let name = "Deepanshi"; // string
+let age = 20;  // number 2^53
+let isStudent = true; // boolean
+let height = 5.5; // float
+let hobbies = ["reading", "coding", "travelling"];
+let address = { city: "Delhi", state: "Delhi" }; // object
+let undefinedVariable; // undefined
+let nullVariable = null; // null // object
+let symbolVariable = Symbol("unique"); // symbol
+
+
