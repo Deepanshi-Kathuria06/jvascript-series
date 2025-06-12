977057e4b4d737560b16889af9a20fea21ae196a
Deepanshi Kathuria <145685048+Deepanshi-Kathuria06@users.noreply.github.com>
2025-06-12 19:30:54 +0000

    javascript variables

---
+++ basics/variable.js
@@ -0,0 +1,19 @@
+// cannot change the value once the value is assign to the variable it will not change
+const accountId = 123456789
+
+
+// accountId = 23  not allowed
+// console.log(accountId)
+
+// can change the value 
+let accountEmail = "deepanshi@gmail.com"
+
+// not used in todays block scope and functional scope problem in var
+var accountPassword = "123456"
+
+// another way of declaration of variable not good to use but you can declare in this way also 
+accountCity = "Delhi"
+
+// when you decalre the varible without the value then the output or value is undefined 
+let accountState;
+console.log(accountState)
