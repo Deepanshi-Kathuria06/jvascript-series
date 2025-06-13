aaeb932e9cac79e7ee1b5574a148c7a8b4b0646c
Deepanshi Kathuria <145685048+Deepanshi-Kathuria06@users.noreply.github.com>
2025-06-13 08:06:20 +0000

    operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -31,7 +31,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "deepanshi" => true
 
 
-let someNumber = 33 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33 
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//***************************** Operations ************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+
+let str1 = "hello"
+let str2 = " deepanshi"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12 
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // 32 
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2 // not good to write
+
+let gameCounter = 100
+++gameCounter; // prefix value incrreases first
+gameCounter++; //  posstfix increment after the usage
+console.log(gameCounter) // 101
+
