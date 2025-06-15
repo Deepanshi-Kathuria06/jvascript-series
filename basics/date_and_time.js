18fefd5b58ac1a705abeb193751c213009ea0be4
Deepanshi Kathuria <145685048+Deepanshi-Kathuria06@users.noreply.github.com>
2025-06-15 16:48:42 +0000

    date and time

---
+++ basics/date_and_time.js
@@ -0,0 +1,12 @@
+// Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString()); // return  Sun Jun 15 2025 11:36:01 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // return  Sun Jun 15 2025
+// console.log(myDate.toISOString()); // return 2025-06-15T11:36:01.145Z
+// console.log(myDate.toLocaleString()); // 6/15/2025, 11:36:01 AM
+
+let myCreatedDate = new Date(2023 , 0, 23) // month starts from 0 
+console.log(myCreatedDate.toDateString())
+
+
