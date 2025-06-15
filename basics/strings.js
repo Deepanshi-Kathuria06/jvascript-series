4f40bb3545b159dcdcac298103c87e408ef95bf9
Deepanshi Kathuria <145685048+Deepanshi-Kathuria06@users.noreply.github.com>
2025-06-15 10:42:40 +0000

    strinigs in java

---
+++ basics/strings.js
@@ -0,0 +1,45 @@
+const name = "Deepanshi"
+
+const age = 20
+
+// console.log(name + age)
+
+//string interploation
+
+// console.log(`hello my name is ${name} and my age is ${age}`)
+
+//string declaration 
+
+const gameName = new String('Deepanshi') //gives in key value pair 
+// console.log(gameName[2])
+// console.log(gameName.__proto__)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt('6'))
+// console.log(gameName.indexOf('p'))
+// console.log(gameName.substring(2,4))
+
+// const stringone = gameName.slice(-4,4) // include negative values
+// console.log(stringone)
+
+// const newStringOne = "  hitesh  "
+// console.log(newStringOne)
+// console.log(newStringOne.trim()) // remove blank spaces
+
+
+//suppose url is there , browser did not notice the blank spaces
+
+// const url ="https://deepanshi.com/deepanshi%20kathuria"
+
+// console.log(url.replace('%20' , '-'))
+
+// console.log(url.includes('deepanshi'))
+// console.log(url.includes('sundar'))
+
+const newname = new String('Deepanshi-kathruia')
+
+console.log(newname.split('-')) // split the string on the base of something
+
+
+
+
+
